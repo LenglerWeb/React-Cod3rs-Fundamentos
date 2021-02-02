aaa423db8b9a4ce4d77e1b094725ff6b883a347e
Eduardo Lengler <lenglerweb@gmail.com>
2021-02-02 19:32:37 -0300

    React JS Curso Rápido ⚛️ Masterclass #1 2020 - 100% Prático! FINALIZADOgit add .

--- src/index.js
+++ src/index.js
@@ -2,24 +2,10 @@ import './index.css'
 import React from 'react'
 import ReactDOM from 'react-dom'
 
- //import Primeiro from './components/Primeiro'
- //import ComParametro from './components/ComParametro'
-import ComFilhos from './components/ComFilhos'
+import App from './App.jsx'
+
 
 ReactDOM.render(
-    <div>
-        <ComFilhos>
-            <ul>
-                <li>Daniele</li>
-                <li>Nancy</li>
-                <li>Amanda</li>
-                <li>Wanda Lúcia</li>
-            </ul>
-        </ComFilhos>
-        
-         {/* <Primeiro />
-         <ComParametro titulo="Esse é o título" subtitulo="Esse é o subtítulo" />
-         <ComParametro titulo="Opa" subtitulo="Epa" /> */}
-    </div>,
+    <App></App>,
     document.getElementById('root')
 )
