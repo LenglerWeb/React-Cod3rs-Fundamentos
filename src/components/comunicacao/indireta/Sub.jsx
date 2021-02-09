efb90268830a18f3937ead9e14ffbfe9e6dd63dc
Lengler Web <lenglerweb@gmail.com>
2021-02-09 16:13:22 -0300

    subindo alterações do DELL AIO-2320

--- src/components/comunicacao/indireta/Sub.jsx
+++ src/components/comunicacao/indireta/Sub.jsx
@@ -2,11 +2,13 @@ import React from 'react';
 
 // eslint-disable-next-line import/no-anonymous-default-export
 export default (props) =>{
+    function acao() {
+        props.onClicar(Math.random(), 'Fim!');
+    }
+
     return (
         <div>
-            <button onClick={() => {
-                props.onClicar(Math.random());
-            } }>Alterar</button>
+            <button onClick={acao}>Alterar</button>
         </div>
     );
 };
--- src/components/comunicacao/indireta/Super.jsx
+++ src/components/comunicacao/indireta/Super.jsx
@@ -1,12 +1,13 @@
 /* eslint-disable import/no-anonymous-default-export */
-import React from 'react';
+import React, {useState} from 'react';
 import Sub from './Sub';
 
 export default (props) => {
 
-    function quandoClicar(valorGerado) {
+    function quandoClicar(valorGerado, texto) {
         console.log('Ação!!!')
         console.log(valorGerado)
+        console.log(texto)
     }
 
     return (
