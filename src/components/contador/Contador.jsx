9730811da4979d04a028df8c39dfee6597271028
Lengler Web <lenglerweb@gmail.com>
2021-02-12 19:52:16 -0300

    Ultima atulização no escritório, antes do carnaval

--- src/components/contador/Contador.jsx
+++ src/components/contador/Contador.jsx
@@ -3,19 +3,34 @@ import React, { Component } from 'react'
 export default class Contador extends Component {
 
     state = {
-        passo: this.props.passo,
-        valor: 0
+        passo: this.props.passo || 1,
+        valor: this.props.valor || 0,
+    }
+
+    inc = () => {
+        this.useState({
+            valor: this.state.valor +1
+        })
+        
+    }
+
+    dec = () => {
+        this.useState({
+            valor: this.state.valor -1
+        })
+        
     }
 
     render() {
         return (
             <div>
                 <h2>Contador</h2>
+                <h4>Passo: {this.state.passo}</h4>
                 <h4>Valor: {this.state.valor}</h4>
 
                 <div>
-                    <button>+</button>
-                    <button>-</button>
+                    <button onClick={this.inc}>+</button>
+                    <button onClick={this.dec}>-</button>
                 </div>
             </div>
         )
