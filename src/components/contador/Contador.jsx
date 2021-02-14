192d180dc2d16921296eb0cbcfac66b00450a346
Eduardo Lengler <lenglerweb@gmail.com>
2021-02-13 21:07:22 -0300

    Finalizado o Contador e iniciando a Mega-Sena

--- src/components/contador/Contador.jsx
+++ src/components/contador/Contador.jsx
@@ -1,6 +1,11 @@
 import './Contador.css'
 import React, { Component } from 'react'
 
+import Display from './Display'
+import PassoForm from './PassoForm'
+import Botoes from './Botoes'
+
+
 export default class Contador extends Component {
 
     state = {
@@ -11,34 +16,32 @@ export default class Contador extends Component {
     inc = () => {
         this.setState({
             valor: this.state.valor + this.state.passo
-        })
-        
+        }) 
     }
 
     dec = () => {
         this.setState({
             valor: this.state.valor - this.state.passo
+        })  
+    }
+
+    mudarPasso = (novoPasso) => {
+        this.setState({
+            passo: novoPasso
         })
-        
     }
 
     render() {
         return (
             <div className='Contador'>
                 <h2>Contador</h2>
-                <div>
-                    <label for="passoInput">Passo: </label>
-                    <input id="passoInput" type="number" 
-                        value={this.state.passo}
-                        onChange={e => this.setState({ passo: +e.target.value })}/>
-                </div>
                 
-                <h4>Valor: {this.state.valor}</h4>
+                <PassoForm passo={this.state.passo}
+                    onPassoChange={this.mudarPasso}></PassoForm>
+                
+                <Display valor={this.state.valor}></Display>
 
-                <div>
-                    <button onClick={this.dec}>-</button>
-                    <button onClick={this.inc}>+</button>
-                </div>
+                <Botoes onInc={this.inc} onDec={this.dec}></Botoes>
             </div>
         )
     }
--- src/components/layout/Card.jsx
+++ src/components/layout/Card.jsx
@@ -4,7 +4,7 @@ import React from 'react'
 export default props =>
     <div className="Card" style={{
         borderColor: props.color || '#000'
-    }}>>
+    }}>
         <div className="Content">
             {props.children}
         </div>
